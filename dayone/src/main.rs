0e28bfe870fec48784f90ec1a3c290f50255535b
Tobias Newmiwaka <newmi1988@gmail.com>
2021-12-01 22:04:27 +0100

    formated code

--- dayone/src/main.rs
+++ dayone/src/main.rs
@@ -1,10 +1,10 @@
-use std::io::BufReader;
-use std::io::BufRead;
-use std::path::Path;
 use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
 use std::io::{Error, ErrorKind};
+use std::path::Path;
 
-fn read_input(path : &Path) -> Result<Vec<i64>, Error> {
+fn read_input(path: &Path) -> Result<Vec<i64>, Error> {
     let file = File::open(path)?;
     let f = BufReader::new(file);
     let mut v = Vec::new();
@@ -20,8 +20,8 @@ fn read_input(path : &Path) -> Result<Vec<i64>, Error> {
     Ok(v)
 }
 
-fn get_increase(v : &Vec<i64>) -> i32 {
-    let mut res : i32 = 0;
+fn get_increase(v: &Vec<i64>) -> i32 {
+    let mut res: i32 = 0;
     let mut last_value = v.first().unwrap().clone();
     for i in v.into_iter() {
         if i > &last_value {
@@ -30,21 +30,21 @@ fn get_increase(v : &Vec<i64>) -> i32 {
         last_value = *i;
     }
 
-    return res
+    return res;
 }
 
-fn count_sliding_window(v : Vec<i64>) -> i32 {
-    let mut cnt : i32 = 0;
-    let mut last_window_sum : i64 = v.iter().take(3).sum();
+fn count_sliding_window(v: Vec<i64>) -> i32 {
+    let mut cnt: i32 = 0;
+    let mut last_window_sum: i64 = v.iter().take(3).sum();
     for w in v.windows(3) {
-        let current_window_sum : i64 = w.iter().sum();
+        let current_window_sum: i64 = w.iter().sum();
         if current_window_sum > last_window_sum {
-            cnt +=1
+            cnt += 1
         }
         last_window_sum = current_window_sum;
     }
 
-    return cnt
+    return cnt;
 }
 
 fn main() {
@@ -56,7 +56,8 @@ fn main() {
     println!("Found {} increases", counter);
 
     let sliding_window_counter = count_sliding_window(vec);
-    println!("Found {} increased in sliding windows", sliding_window_counter);
-
+    println!(
+        "Found {} increased in sliding windows",
+        sliding_window_counter
+    );
 }
-
