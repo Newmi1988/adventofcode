5f8d2297c61ee1110b1fdb4ab9d586c4bc48fec0
Tobias Newmiwaka <newmi1988@gmail.com>
2021-12-01 22:00:09 +0100

    solution to day one

---
+++ ReadMe.md
@@ -0,0 +1,7 @@
+# Solutions to Advent of Code 🎄🎄🎄
+
+My solutions to advent of code.
+
+All written in Rust (I'm a rust beginner so it was done for practice and the solution may not be optimal regarding runtime and style)
+
+Every day is wirtten as its own cargo in a subfolder. The input is ignored. 
---
+++ dayone/src/main.rs
@@ -0,0 +1,62 @@
+use std::io::BufReader;
+use std::io::BufRead;
+use std::path::Path;
+use std::fs::File;
+use std::io::{Error, ErrorKind};
+
+fn read_input(path : &Path) -> Result<Vec<i64>, Error> {
+    let file = File::open(path)?;
+    let f = BufReader::new(file);
+    let mut v = Vec::new();
+
+    for line in f.lines() {
+        let line = line?;
+        let i = line
+            .trim()
+            .parse()
+            .map_err(|e| Error::new(ErrorKind::InvalidData, e))?;
+        v.push(i)
+    }
+    Ok(v)
+}
+
+fn get_increase(v : &Vec<i64>) -> i32 {
+    let mut res : i32 = 0;
+    let mut last_value = v.first().unwrap().clone();
+    for i in v.into_iter() {
+        if i > &last_value {
+            res += 1;
+        }
+        last_value = *i;
+    }
+
+    return res
+}
+
+fn count_sliding_window(v : Vec<i64>) -> i32 {
+    let mut cnt : i32 = 0;
+    let mut last_window_sum : i64 = v.iter().take(3).sum();
+    for w in v.windows(3) {
+        let current_window_sum : i64 = w.iter().sum();
+        if current_window_sum > last_window_sum {
+            cnt +=1
+        }
+        last_window_sum = current_window_sum;
+    }
+
+    return cnt
+}
+
+fn main() {
+    let path = Path::new("./input");
+    let vec = read_input(path).unwrap();
+
+    let counter = get_increase(&vec);
+
+    println!("Found {} increases", counter);
+
+    let sliding_window_counter = count_sliding_window(vec);
+    println!("Found {} increased in sliding windows", sliding_window_counter);
+
+}
+
