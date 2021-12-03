198bfcc96b47427eb8b576fa5eb42f80c606f297
Tobias Newmiwaka <newmi1988@gmail.com>
2021-12-03 22:15:06 +0100

    day 3: solved part 1

--- daythree/src/main.rs
+++ daythree/src/main.rs
@@ -1,11 +1,10 @@
+use std::char;
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
-use std::io::{Error};
+use std::io::Error;
 use std::path::Path;
-use std::char;
-
 
 fn read_input(path: &Path) -> Result<Vec<String>, Error> {
     let file = File::open(path)?;
@@ -20,28 +19,28 @@ fn read_input(path: &Path) -> Result<Vec<String>, Error> {
     Ok(v)
 }
 
-fn gamma_rate(v : &Vec<String>) -> i32 {
+fn binary_diagnostic(v: &Vec<String>) -> i32 {
     let string_length = v.first().unwrap().len();
     let mut vhm = Vec::new();
     for _ in 0..string_length {
         let mut hm = HashMap::new();
-        hm.insert('0',0);
-        hm.insert('1',0);
+        hm.insert('0', 0);
+        hm.insert('1', 0);
         vhm.push(hm)
     }
     for b in v.iter() {
-        for(i,v) in b.chars().enumerate() {
+        for (i, v) in b.chars().enumerate() {
             let counter = vhm[i].entry(v).or_insert(0);
             *counter += 1;
         }
     }
 
-    // set_max = 
-    let mut max : Vec<char> = Vec::new();
-    let mut min : Vec<char> = Vec::new();
+    // set_max =
+    let mut max: Vec<char> = Vec::new();
+    let mut min: Vec<char> = Vec::new();
 
     for h in vhm {
-        println!("{:?}",h);
+        println!("{:?}", h);
 
         if h[&'0'] > h[&'1'] {
             max.push('0');
@@ -52,11 +51,15 @@ fn gamma_rate(v : &Vec<String>) -> i32 {
         }
     }
 
-    println!("{:?}", max);
-    println!("{:?}", min);
+    let max_str: String = max.into_iter().collect();
+    let min_str: String = min.into_iter().collect();
 
-    let gamma : i32 = 0;
-    return gamma
+    println!("{}", max_str);
+    println!("{}", min_str);
+
+    let gamma: i32 = isize::from_str_radix(&max_str, 2).unwrap() as i32;
+    let epsilon: i32 = isize::from_str_radix(&min_str, 2).unwrap() as i32;
+    return gamma * epsilon;
 }
 
 fn main() {
@@ -64,7 +67,6 @@ fn main() {
     let inputs = read_input(path).unwrap();
 
     // part 1
-    // println!("{:?}", inputs);
-
-    let i = gamma_rate(&inputs);
+    let i = binary_diagnostic(&inputs);
+    println!("{}", i);
 }
