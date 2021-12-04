b6ad9f9a6621ecee35cad326b9270732e92be9f3
Tobias Newmiwaka <newmi1988@gmail.com>
2021-12-04 22:09:39 +0100

    fmt and added timer

--- dayfour/src/main.rs
+++ dayfour/src/main.rs
@@ -3,68 +3,64 @@ use std::io::BufRead;
 use std::io::BufReader;
 use std::io::Error;
 use std::path::Path;
+use std::time::{Instant};
 
 #[derive(Debug)]
 struct Board {
     width: usize,
     heigth: usize,
     values: Vec<Vec<i32>>,
-    wining_number : i32,
-    solved : bool
+    wining_number: i32,
+    solved: bool,
 }
 
 impl Board {
     // implement col and row checking for win
-    fn is_solved(&mut self,n: Vec<i32>) -> bool {
+    fn is_solved(&mut self, n: Vec<i32>) -> bool {
         if self.solved {
             return self.solved;
         }
 
-        
         // go throgh rows and check if in n
         for row in &self.values {
             if row.iter().all(|item| n.contains(item)) {
-                println!("{:?}", self.values);
                 self.solved = true;
                 self.wining_number = *n.last().unwrap();
-                println!("Game in row {:?} solved with inputs {:?}", row, n);
             }
         }
 
         // go through columns check if in n
         for i in 0..self.heigth {
-            let mut column : Vec<i32> = Vec::new();
+            let mut column: Vec<i32> = Vec::new();
             for j in 0..self.width {
                 column.push(self.values[j][i])
             }
 
             if column.iter().all(|item| n.contains(item)) {
-                println!("{:?}", self.values);
                 self.solved = true;
                 self.wining_number = *n.last().unwrap();
-                println!("Game in column {:?} solved with inputs {:?}", column, n);
             }
         }
 
-        return self.solved
+        return self.solved;
     }
 
-    fn get_answer(&self, n : Vec<i32>) -> i32 {
+    fn get_answer(&self, n: Vec<i32>) -> i32 {
         // get all values not in solution
-        let mut f : Vec<i32> = self.values
+        let mut f: Vec<i32> = self
+            .values
             .iter()
             .flat_map(|array| array.iter())
             .cloned()
             .collect();
 
+        f.retain(|&x| !n.contains(&x));
 
-        f.retain(|&x| !n.contains(&x) );
-
-        let f_sum : i32 = f.iter().sum();
+        let f_sum: i32 = f.iter().sum();
         // println!("Sum : {}, Winning_number : {}", f_sum, self.wining_number);
-        let res : i32 = self.wining_number * f_sum;
+        let res: i32 = self.wining_number * f_sum;
 
-        return res
+        return res;
     }
 }
 
@@ -103,13 +99,13 @@ impl Games {
 
         let mut boards: Vec<Board> = Vec::new();
         for b in board_lines.chunks(dimensions) {
-            let mut board : Board = Board {
-            width: dimensions,
-            heigth: dimensions,
-            values: b.to_vec(),
-            solved : false,
-            wining_number: 0
-        };
+            let board: Board = Board {
+                width: dimensions,
+                heigth: dimensions,
+                values: b.to_vec(),
+                solved: false,
+                wining_number: 0,
+            };
             boards.push(board)
         }
 
@@ -122,21 +118,21 @@ impl Games {
     }
 
     fn get_first_solved(&mut self) -> i32 {
-        let mut res : i32 = 0;
+        let res: i32 = 0;
         for i in 5..self.inputs.len() {
             for board in &mut self.boards {
                 if board.is_solved(self.inputs[0..i].to_vec()) {
-                    let s : i32 = board.get_answer(self.inputs[0..i].to_vec());
-                    return s
+                    let s: i32 = board.get_answer(self.inputs[0..i].to_vec());
+                    return s;
                 }
             }
         }
-        
-        return res
+
+        return res;
     }
-    
+
     fn get_last_solved(&mut self) -> i32 {
-        let mut res : i32 = 0;
+        let mut res: i32 = 0;
         for i in 5..self.inputs.len() {
             for board in &mut self.boards {
                 if board.is_solved(self.inputs[0..i].to_vec()) {
@@ -147,7 +143,7 @@ impl Games {
             self.boards.retain(|x| x.solved == false)
         }
 
-        return res
+        return res;
     }
 }
 
@@ -156,10 +152,12 @@ fn main() {
     let mut g = Games::from_input_file(path).unwrap();
 
     // part 1
+    let part_one_time = Instant::now();
     let answer_part_1 = g.get_first_solved();
-    println!("{}",answer_part_1);
+    println!("Answer : {} ({}µs)", answer_part_1, part_one_time.elapsed().as_micros());
 
     // part 2
+    let part_two_time = Instant::now();
     let answer_part_2 = g.get_last_solved();
-    println!("{}",answer_part_2)
+    println!("Answer : {} ({}µs)", answer_part_2, part_two_time.elapsed().as_micros())
 }
