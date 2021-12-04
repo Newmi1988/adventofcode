954e8b8298ae880ce9bc05866b849664f4459802
Tobias Newmiwaka <newmi1988@gmail.com>
2021-12-04 09:42:35 +0100

    still error in part2

--- daythree/src/main.rs
+++ daythree/src/main.rs
@@ -19,9 +19,9 @@ fn read_input(path: &Path) -> Result<Vec<String>, Error> {
     Ok(v)
 }
 
-fn binary_diagnostic(v: &Vec<String>) -> i32 {
-    let string_length = v.first().unwrap().len();
+fn get_binary_position_counts(v: &Vec<String>) -> Vec<HashMap<char,i32>> {
     let mut vhm = Vec::new();
+    let string_length = v.first().unwrap().len();
     for _ in 0..string_length {
         let mut hm = HashMap::new();
         hm.insert('0', 0);
@@ -35,12 +35,19 @@ fn binary_diagnostic(v: &Vec<String>) -> i32 {
         }
     }
 
+    return vhm
+}
+
+fn binary_diagnostic(v: &Vec<String>) -> i32 {
+    let vhm = get_binary_position_counts(v);
+    // println!("{:?}", vhm);
+
     // set_max =
     let mut max: Vec<char> = Vec::new();
     let mut min: Vec<char> = Vec::new();
 
     for h in vhm {
-        println!("{:?}", h);
+        // println!("{:?}", h);
 
         if h[&'0'] > h[&'1'] {
             max.push('0');
@@ -54,19 +61,67 @@ fn binary_diagnostic(v: &Vec<String>) -> i32 {
     let max_str: String = max.into_iter().collect();
     let min_str: String = min.into_iter().collect();
 
-    println!("{}", max_str);
-    println!("{}", min_str);
 
-    let gamma: i32 = isize::from_str_radix(&max_str, 2).unwrap() as i32;
-    let epsilon: i32 = isize::from_str_radix(&min_str, 2).unwrap() as i32;
+    let gamma: i32 = i32::from_str_radix(&max_str, 2).unwrap();
+    let epsilon: i32 = i32::from_str_radix(&min_str, 2).unwrap();
+
+    println!("{}", gamma);
+    println!("{}", epsilon);
+
     return gamma * epsilon;
+}
+
+fn co_two_scrubber_rating(v : &mut Vec<String>) -> i32 {
+    let vhm = get_binary_position_counts(v);
+
+    // println!("{:?}", vhm);
+
+    let oxygen = v;
+    let mut co2scrub = oxygen.clone();
+    // use the counts to sort the values into 
+    for (i,h) in vhm.iter().enumerate() {
+
+        // println!("{} - {:?}",i,h);
+
+        let mut max : char = '0';
+        let mut min : char = '1';
+        if h[&'0'] < h[&'1'] {
+            max = '1';
+            min = '0';
+        }
+        
+        if oxygen.len() > 1 {
+            oxygen.retain(|x| x.chars().nth(i).unwrap() == max);
+        }
+        if co2scrub.len() > 1 {
+            co2scrub.retain(|x| x.chars().nth(i).unwrap() == min);
+        }
+        // println!("{:?}",oxygen);
+    }
+
+    // println!("{}", oxygen[0]);
+    // println!("{}", co2scrub[0]);
+    println!("{}",oxygen[0]);
+    println!("{}",co2scrub[0]);
+
+    let oxygen_value : i32 = i32::from_str_radix(&oxygen[0],2).unwrap();
+    let co2scrub_value : i32 = i32::from_str_radix(&co2scrub[0],2).unwrap();
+
+    println!("{}", oxygen_value);
+    println!("{}", co2scrub_value);
+    
+    return oxygen_value*co2scrub_value
 }
 
 fn main() {
     let path = Path::new("./input");
-    let inputs = read_input(path).unwrap();
+    let mut inputs = read_input(path).unwrap();
 
     // part 1
     let i = binary_diagnostic(&inputs);
-    println!("{}", i);
+    println!("Part 1: {}", i);
+
+    // part 2
+    let j = co_two_scrubber_rating(&mut inputs);
+    println!("Part 2: {}",j);
 }
