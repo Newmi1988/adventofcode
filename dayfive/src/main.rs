0cd2989cfcbdbea1681ee387f3a42ecc2598bfef
Tobias Newmiwaka <newmi1988@gmail.com>
2021-12-11 19:33:11 +0100

    day 5 solved

--- dayfive/src/main.rs
+++ dayfive/src/main.rs
@@ -2,8 +2,9 @@ use std::collections::HashMap;
 use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
-use std::io::{Error, ErrorKind};
+use std::io::Error;
 use std::path::Path;
+use std::time::Instant;
 
 #[derive(Debug, Copy, Clone, Hash)]
 struct Position {
@@ -13,7 +14,7 @@ struct Position {
 
 impl Position {
     fn new() -> Position {
-        Position{ x : 0, y : 0}
+        Position { x: 0, y: 0 }
     }
 }
 
@@ -22,7 +23,6 @@ impl PartialEq for Position {
         self.x == other.x && self.y == other.y
     }
 }
-
 
 impl Eq for Position {}
 
@@ -42,8 +42,16 @@ impl Line {
         return self.start.y == self.end.y;
     }
 
+    fn slope(self) -> f32 {
+        (self.end.y as f32 - self.start.y as f32) / (self.start.x as f32 - self.end.x as f32)
+    }
+
     fn is_diag(self) -> bool {
-        return ((self.start.y - self.end.y).abs() / (self.start.x - self.end.x).abs()) == 1
+        if self.is_horizontal() || self.is_vertical() {
+            return false;
+        } else {
+            return self.slope().abs() as i32 == 1;
+        }
     }
 }
 
@@ -53,16 +61,15 @@ impl Iterator for Line {
     fn next(&mut self) -> Option<Self::Item> {
         let mut p = Position::new();
         if self.is_horizontal() {
-            // println!("{:?}", self);
             if self.index > (self.end.x - self.start.x).abs() as usize {
                 return None;
             } else {
                 if self.start.x > self.end.x {
-                        p.x = self.start.x - self.index as i32;
-                        p.y = self.start.y;
+                    p.x = self.start.x - self.index as i32;
+                    p.y = self.start.y;
                 } else {
-                        p.x= self.start.x + self.index as i32;
-                        p.y = self.start.y;
+                    p.x = self.start.x + self.index as i32;
+                    p.y = self.start.y;
                 }
             }
         } else if self.is_vertical() {
@@ -70,14 +77,31 @@ impl Iterator for Line {
                 return None;
             } else {
                 if self.start.y > self.end.y {
-                        p.x=self.start.x;
-                        p.y= self.start.y - self.index as i32;
+                    p.x = self.start.x;
+                    p.y = self.start.y - self.index as i32;
                 } else {
-                        p.x=self.start.x;
-                        p.y= self.start.y + self.index as i32;
+                    p.x = self.start.x;
+                    p.y = self.start.y + self.index as i32;
+                }
+            }
+        } else if self.is_diag() {
+            if self.index > (self.end.x - self.start.x).abs() as usize {
+                return None;
+            } else {
+                if self.start.x > self.end.x {
+                    p.x = self.start.x - self.index as i32;
+                } else {
+                    p.x = self.start.x + self.index as i32;
+                }
+
+                if self.start.y > self.end.y {
+                    p.y = self.start.y - self.index as i32;
+                } else {
+                    p.y = self.start.y + self.index as i32;
                 }
             }
         } else {
+            println!("Filter not working");
             return None;
         }
         self.index += 1;
@@ -114,9 +138,7 @@ fn read_input(path: &Path) -> Result<Vec<Line>, Error> {
 }
 
 fn part1(lines: &mut Vec<Line>) -> i32 {
-    lines.retain(|line| line.start.x == line.end.x || line.start.y == line.end.y);
-
-    println!("{}", lines.len());
+    lines.retain(|line| line.is_horizontal() || line.is_vertical());
 
     let mut points: HashMap<Position, i32> = HashMap::new();
     for line in lines {
@@ -127,26 +149,46 @@ fn part1(lines: &mut Vec<Line>) -> i32 {
             let counter = points.entry(pos).or_insert(0);
             *counter += 1
         }
-
     }
-    points.retain(|_,v| *v >= 2);
+    points.retain(|_, v| *v >= 2);
     points.len() as i32
 }
 
-// fn part2(lines: &mut Vec<Line>) -> i32 {
-//     lines.retain(|line| *line.is_vertical())
+fn part2(lines: &mut Vec<Line>) -> i32 {
+    lines.retain(|line| line.is_vertical() || line.is_horizontal() || line.is_diag());
 
-//     0_i32
-// }
+    let mut points: HashMap<Position, i32> = HashMap::new();
+    for line in lines {
+        for pos in line {
+            // add counts to position
+            let counter = points.entry(pos).or_insert(0);
+            *counter += 1
+        }
+    }
+    points.retain(|_, v| *v >= 2);
+    points.len() as i32
+}
 
 fn main() {
     let path = Path::new("./input");
-    let mut lines = read_input(path).unwrap();
-    println!("{}", lines.len());
-
-    // println!("{:?}", lines);
-
+    
     // part 1
-    let overlaps = part1(&mut lines);
-    println!("Overlaps : {}", overlaps);
+    let mut lines = read_input(path).unwrap();
+    let part_one_time = Instant::now();
+    let answer_part_one = part1(&mut lines);
+    println!(
+        "Answer : {} ({}µs)",
+        answer_part_one,
+        part_one_time.elapsed().as_micros()
+    );
+
+    //part2
+    let mut lines = read_input(path).unwrap();
+    let part_two_time = Instant::now();
+    let answer_part_two = part2(&mut lines);
+    println!(
+        "Answer : {} ({}µs)",
+        answer_part_two,
+        part_two_time.elapsed().as_micros()
+    )
 }
