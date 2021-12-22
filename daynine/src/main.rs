2eebf884bd58c1ca5b2076a3cd2052f0c703116a
Tobias Newmiwaka <newmi1988@gmail.com>
2021-12-22 20:19:02 +0100

    fixed types and return

--- daynine/src/main.rs
+++ daynine/src/main.rs
@@ -6,40 +6,40 @@ use std::io::Error;
 use std::path::Path;
 use std::time::Instant;
 
-fn read_input(path: &Path) -> Result<i32,Error> {
+fn read_input(path: &Path) -> Result<Vec<Vec<u32>>,Error> {
     let file = File::open(path)?;
     let f = BufReader::new(file);
-
-    let mut heightmap : Vec<Vec<u16>> = Vec::new();
+ 
+    const RADIX: u32 = 10;
+    let mut heightmap : Vec<Vec<u32>> = Vec::new();
 
     for line in f.lines() {
         let line = line?;
-        let mut row : Vec<u16> = Vec::new();
+        let mut row : Vec<u32> = Vec::new();
         let t : String = String::from(line);
-        println!("{}",t);
         for c in t.chars() {
-            println!("{}",c);
-            let z = (c.to_string()).parse::<u16>().unwrap();
-            row.push(z);
-            println!("{:?}",row);
+            row.push(c
+                .to_digit(RADIX)
+                .unwrap()
+            )
         }
         heightmap.push(row);
     }
 
     println!("{:?}",heightmap);
     
-    Ok(0_i32)
+    Ok(heightmap)
 }
 
-// fn part1(heightmap: &Vec<Vec<u8>>
+// fn part1(heightmap: &Vec<Vec<u32>>) -> u32 {
 
-
+// }
 
 
 fn main() {
 
     let path = Path::new("./input");
 
-    let i: Result<i32, Error> = read_input(path);
+    let i: Result<Vec<Vec<u32>>, Error> = read_input(path);
 
 }
