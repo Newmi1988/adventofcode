59a6c2ffefeea20bdd86d8cdaeef489331b2efef
Tobias Newmiwaka <newmi1988@gmail.com>
2021-12-26 20:50:30 +0100

    Day 9: Solution part 2

--- daynine/src/main.rs
+++ daynine/src/main.rs
@@ -1,12 +1,9 @@
 use core::panic;
-use std::cmp;
 use std::collections::HashMap;
-use std::collections::HashSet;
 use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
 use std::io::Error;
-use std::ops::Sub;
 use std::path::Path;
 use std::time::Instant;
 use std::vec;
@@ -67,8 +64,6 @@ fn part1(heightmap: &Vec<Vec<u32>>) -> u32 {
             if current_position_value == *window_min
                 && search_window.values().max().unwrap() != window_min
             {
-                println!("{:?}", search_window);
-                println!("Minimum found : {}", current_position_value);
                 found_minumum.push(current_position_value)
             }
         }
@@ -79,6 +74,90 @@ fn part1(heightmap: &Vec<Vec<u32>>) -> u32 {
     }
 
     found_minumum.iter().sum()
+}
+
+#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
+struct Area {
+    start_x: usize,
+    end_x: usize,
+    start_y: usize,
+    end_y: usize,
+    area: u32,
+}
+
+fn search_area(
+    heightmap: &Vec<Vec<u32>>,
+    visited: &mut HashMap<(usize, usize), bool>,
+    x: usize,
+    y: usize,
+) {
+    if heightmap[x][y] < 9 && visited.contains_key(&(x, y)) == false {
+        visited.insert((x, y), true);
+        // search the 4 neighbors
+        search_area(heightmap, visited, x + 1, y);
+        search_area(heightmap, visited, x - 1, y);
+        search_area(heightmap, visited, x, y + 1);
+        search_area(heightmap, visited, x, y - 1);
+    }
+}
+
+fn fill_arrea(heightmap: &Vec<Vec<u32>>, x: usize, y: usize) -> HashMap<(usize, usize), bool> {
+    let mut possible_area_positions: HashMap<(usize, usize), bool> = HashMap::new();
+
+    search_area(heightmap, &mut possible_area_positions, x, y);
+
+    possible_area_positions
+}
+
+fn get_area(heightmap: &Vec<Vec<u32>>, x: usize, y: usize) -> Option<Area> {
+    if heightmap[x][y] > 8 {
+        return None;
+    } else {
+        let visited_positions: HashMap<(usize, usize), bool> = fill_arrea(heightmap, x, y);
+        let mut x_values: Vec<usize> = Vec::new();
+        let mut y_values: Vec<usize> = Vec::new();
+        for tuple in visited_positions.keys() {
+            x_values.push(tuple.0);
+            y_values.push(tuple.1);
+        }
+
+        if visited_positions.len() <= 1 {
+            None
+        } else {
+            Some(Area {
+                start_x: x_values.iter().min().unwrap_or(&0).clone(),
+                end_x: x_values.iter().max().unwrap_or(&0).clone(),
+                start_y: y_values.iter().min().unwrap_or(&0).clone(),
+                end_y: y_values.iter().min().unwrap_or(&0).clone(),
+                area: visited_positions.len() as u32,
+            })
+        }
+    }
+}
+
+fn part2(heightmap: &Vec<Vec<u32>>) -> u32 {
+    // hashmap to keep track of sizes
+    let mut areas: HashMap<Area, u32> = HashMap::new();
+
+    for i in 1..(heightmap.len() - 1) {
+        for j in 1..(heightmap[0].len() - 1) {
+            // find basins
+            if let Some(x) = get_area(heightmap, i, j) {
+                areas.insert(x, x.area);
+            }
+        }
+    }
+
+    // sort the areas and then get the three largest
+    let mut areas_values: Vec<u32> = Vec::new();
+    for v in areas.values() {
+        areas_values.push(*v)
+    }
+
+    areas_values.sort();
+    areas_values.reverse();
+
+    areas_values[0..=2].iter().product()
 }
 
 fn main() {
@@ -96,5 +175,13 @@ fn main() {
         "Result Part 1: {} ({}µs)",
         part_one_result,
         part_one_time.elapsed().as_micros()
+    );
+
+    let part_two_time: Instant = Instant::now();
+    let part_two_result: u32 = part2(&input_heightmap);
+    println!(
+        "Result Part 2: {} ({}µs)",
+        part_two_result,
+        part_two_time.elapsed().as_micros()
     );
 }
